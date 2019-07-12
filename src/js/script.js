87b2518aeec734bbeec47476ef2023a501a93f06
kunak <kunak.n@gmail.com>
2019-07-12 19:15:50 +0300

    Last changes in sections: header, rev-editor, advantages, prices

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,16 @@
-let burgerBtn = document.getElementById("burger-button");
-let mainMenu = document.getElementById("main-menu");
+// document.addEventListener("DOMContentLoaded", function () {
+    let burgerBtn = document.getElementById("burger-button");
+    let mainMenu = document.getElementById("main-menu");
 
-burgerBtn.addEventListener("click", function () {
-    mainMenu.classList.toggle("main-menu--animated");
-});
+    mainMenu.classList.remove('main-menu--animated');
 
+    burgerBtn.addEventListener("click", function () {
+        mainMenu.classList.toggle("main-menu--animated");
+
+        // if (mainMenu.classList.contains("main-menu--animated")) {
+        //     document.body.addEventListener(`mouseup`, function () {
+        //         mainMenu.classList.remove('main-menu--animated');
+        //     });
+        // }
+    });
+// });
